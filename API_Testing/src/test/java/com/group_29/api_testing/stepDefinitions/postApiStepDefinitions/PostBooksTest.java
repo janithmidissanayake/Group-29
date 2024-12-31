8cc3920619d4a328ae2e299b1018178fa7e087f7
Charuka Nedungamuwa <charunadungamuwa@gmail.com>
2024-12-31 08:44:23 +0530

    commit 5 test cases

--- API_Testing/src/test/java/com/group_29/api_testing/stepDefinitions/postApiStepDefinitions/PostBooksTest.java
+++ API_Testing/src/test/java/com/group_29/api_testing/stepDefinitions/postApiStepDefinitions/PostBooksTest.java
@@ -23,12 +23,28 @@ public class PostBooksTest {
         RestAssured.baseURI = "http://localhost:7081"; // Base URI for API
     }
 
-    @When("I send a POST request to {string} with the book details")
-    public void iSendAPostRequestToWithTheBookDetails(String apiPath) {
+    @When("I send a POST request to {string} with the book details as user")
+    public void iSendAPostRequestToWithTheBookDetailsAsUser(String apiPath) {
         // Sample request body
         String requestBody = "{\n" +
                 "  \"title\": \"New Book\",\n" +
                 "  \"author\": \"Author Name\"\n" +
+                "}";
+
+        // Sending POST request with user credentials
+        response = given()
+                .auth().basic("user", "password")  // Use "user" credentials here
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post(apiPath);
+    }
+    @When("I send a POST request to {string} with the book details as admin")
+    public void iSendAPostRequestToWithTheBookDetailsAsAdmin(String apiPath) {
+        // Sample request body
+        String requestBody = "{\n" +
+                "  \"title\": \"New Book two\",\n" +
+                "  \"author\": \"Author Name two\"\n" +
                 "}";
 
         // Sending POST request based on user credentials
@@ -39,7 +55,70 @@ public class PostBooksTest {
                 .when()
                 .post(apiPath);
     }
+    @When("I send a POST request to {string} with an integer as the author")
+    public void iSendAPostRequestToWithAnIntegerAsTheAuthor(String apiPath) {
+        // Request body with an integer for the "author" field
+        String requestBody = "{\n" +
+                "  \"title\": \"Book With Invalid Author\",\n" +
+                "  \"author\": 12345\n" +
+                "}";
 
+        // Sending POST request
+        response = given()
+                .auth().basic("admin", "password")  // Assuming admin credentials
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post(apiPath);
+    }
+
+
+    @When("I send a POST request to {string} with the book details and additional attributes")
+    public void iSendAPostRequestToWithTheBookDetailsAndAdditionalAttributes(String apiPath) {
+        // Request body with additional unexpected attribute
+        String requestBody = "{\n" +
+                "  \"title\": \"New Book\",\n" +
+                "  \"author\": \"Author Name\",\n" +
+                "  \"extraAttribute\": \"Unexpected Value\"\n" +
+                "}";
+
+        // Sending POST request
+        response = given()
+                .auth().basic("admin", "password")
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post(apiPath);
+    }
+    @When("I send a POST request to {string} with missing values")
+    public void iSendAPostRequestToWithMissingValues(String apiPath) {
+        String requestBody = "{\n" +
+                "  \"title\": null,\n" +
+                "  \"author\": null\n" +
+                "}";
+
+        response = given()
+                .auth().basic("admin", "password")
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post(apiPath);
+    }
+
+@Then("the response should contain the message {string}")
+public void theResponseShouldContainTheMessage(String expectedMessage) {
+    // Extract the actual message from the response body
+//    String actualMessage = response.jsonPath().getString("message");
+    String actualMessage = response.asString();
+
+    Assert.assertEquals(actualMessage, expectedMessage, "The error message does not match.");
+}
+    @Then("the response should contain an error message {string}")
+    public void theResponseShouldContainAnErrorMessage(String errorMessage) {
+        // Validate that the response contains the expected error message
+        Assert.assertTrue(response.body().asString().contains(errorMessage),
+                "Expected error message not found in response: " + errorMessage);
+    }
     @Then("I should receive a response with status code {int}")
     public void iShouldReceiveAResponseWithStatusCode(int statusCode) {
         response.then().statusCode(statusCode); // Validate status code
--- API_Testing/src/test/resources/features/PostBook.feature
+++ API_Testing/src/test/resources/features/PostBook.feature
@@ -2,11 +2,32 @@ Feature: Add new book to the system
 
   Scenario: Admin adds a new book successfully
     Given I am an admin user
-    When I send a POST request to "/api/books" with the book details
+    When I send a POST request to "/api/books" with the book details as admin
     Then I should receive a response with status code 201
     And the response should contain the book's id
 
-  Scenario: User tries to add a new book (Unauthorized)
+  Scenario: User tries to add a new book
     Given I am a user
-    When I send a POST request to "/api/books" with the book details
-    Then I should receive a response with status code 208
+    When I send a POST request to "/api/books" with the book details as user
+    Then I should receive a response with status code 201
+    And the response should contain the book's id
+
+  Scenario: Admin tries to add a book with an integer as the author
+    Given I am an admin user
+    When I send a POST request to "/api/books" with an integer as the author
+    Then I should receive a response with status code 400
+    And the response should contain an error message "Invalid data type for author"
+
+
+  Scenario: Admin adds a book with unexpected attributes in the payload
+    Given I am an admin user
+    When I send a POST request to "/api/books" with the book details and additional attributes
+    Then I should receive a response with status code 400
+    And the response should contain an error message "Unexpected attribute in request body"
+
+
+  Scenario: Admin sends a POST request with missing values in the payload
+    Given I am an admin user
+    When I send a POST request to "/api/books" with missing values
+    Then I should receive a response with status code 400
+    And the response should contain the message "Fields 'title' and 'author' cannot be null or empty"
