70b5f34cb26baa515be65937be724052bf85d944
janithmidissanayake <dissanayakejw.20@uom.lk>
2025-01-01 20:38:56 +0530

    test case implementation

--- API_Testing/src/test/java/com/group_29/api_testing/stepDefinitions/updateApiStepDefinitions/UpdateBookStepDefinitions.java
+++ API_Testing/src/test/java/com/group_29/api_testing/stepDefinitions/updateApiStepDefinitions/UpdateBookStepDefinitions.java
@@ -1,6 +1,8 @@
 package com.group_29.api_testing.stepDefinitions.updateApiStepDefinitions;
 
 
+import com.group_29.utils.ApiUtils;
+import com.group_29.utils.ConfigLoader;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -11,57 +13,48 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 
-
 public class UpdateBookStepDefinitions {
     private Response response;
     private int bookId;
 
     @Given("the API base URL is {string}")
     public void setBaseUrl(String baseUrl) {
-        RestAssured.baseURI = baseUrl;
+        RestAssured.baseURI = ConfigLoader.getProperty("base_url");
     }
 
     @Given("a book exists with id {int}")
     public void aBookExistsWithId(int id) {
         this.bookId = id;
-        // TODO: Implement a check to ensure the book exists
+        String endpoint = "/api/books/" + id;
+        Response bookResponse = ApiUtils.sendGetRequest(endpoint, ConfigLoader.getProperty("valid_admin_username"), ConfigLoader.getProperty("valid_admin_password"));
     }
 
     @When("I send a PUT request to {string} with the following payload")
     public void iSendAPutRequestToWithTheFollowingPayload(String endpoint, String payload) {
-        RequestSpecification request = RestAssured.given()
-                .auth()
-                .basic("admin", "password")
-                .header("Content-Type", "application/json")
-                .body(payload);
-        response = request.put(endpoint);
+        String fullEndpoint = RestAssured.baseURI + endpoint;
+        RequestSpecification request = ApiUtils.sendPutRequest(endpoint,ConfigLoader.getProperty("valid_admin_username"),ConfigLoader.getProperty("valid_admin_password"),payload);
+        response = request.put(fullEndpoint);
         System.out.println("Response status code: " + response.getStatusCode());
         System.out.println("Response body: " + response.getBody().asString());
     }
+
     @When("I send a PUT request to {string} as a user with the following payload")
     public void sendAPutRequestAsUser(String endpoint, String payload) {
-        RequestSpecification request = RestAssured.given()
-                .auth()
-                .basic("user", "password") // Ensure the API supports Basic Authentication
-                .header("Content-Type", "application/json")
-                .body(payload);
-
-        response = request.put(endpoint); // Ensure 'response' is properly scoped
+        String fullEndpoint = RestAssured.baseURI + endpoint;
+        RequestSpecification request = ApiUtils.sendPutRequest(endpoint,ConfigLoader.getProperty("valid_user_username"),ConfigLoader.getProperty("valid_user_password"),payload);
+        response = request.put(fullEndpoint);
         System.out.println("Response status code: " + response.getStatusCode());
         System.out.println("Response body: " + response.getBody().asString());
     }
-
-
 
     @Then("the response status code should be {int}")
     public void verifyStatusCode(int expectedStatusCode) {
-        int actualResponse  = response.getStatusCode();
+        int actualResponse = response.getStatusCode();
         if (actualResponse != expectedStatusCode) {
             Assert.fail("BUG: Expected status code " + expectedStatusCode + " but got " + actualResponse);
         }
         Assert.assertEquals(expectedStatusCode, actualResponse);
     }
-
 
     @And("the response body should contain:")
     public void theResponseBodyShouldContain(String expectedResponseBody) {
@@ -81,19 +74,18 @@ public class UpdateBookStepDefinitions {
         Assert.assertTrue(actualResponseBody.contains(expectedErrorMessage.trim()),
                 "Error message not found in response");
     }
+
     @And("the response body should contain invalid id error message:")
     public void verifyInvalidIdResponseBody(String expectedErrorMessage) {
         String actualResponseBody = response.getBody().asString();
         Assert.assertTrue(actualResponseBody.contains(expectedErrorMessage.trim()),
                 "Error message not found in response");
     }
+
     @And("the response body should contain an authorization error message:")
     public void verifyUnauthorizedAccessResponseBody(String expectedErrorMessage) {
         String actualResponseBody = response.getBody().asString();
-        System.out.println("jjjjjjjjj"+ actualResponseBody);
         Assert.assertTrue(actualResponseBody.contains(expectedErrorMessage.trim()),
                 "Error message not found in response");
     }
-
-
 }
