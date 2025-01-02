5cd8a05b627454173038e9ed66732e0c1fad6f96
Charuka Nedungamuwa <charunadungamuwa@gmail.com>
2025-01-02 20:06:26 +0530

    commit ui test cases

--- UI_Testing/src/main/java/com/group_29/ui_testing/pages/ProductsPage.java
+++ UI_Testing/src/main/java/com/group_29/ui_testing/pages/ProductsPage.java
@@ -5,10 +5,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class ProductsPage {
     @FindBy(className = "title")
     private WebElement pageTitle;
 
+    @FindBy(xpath = "//button[text()='Add to cart']")
+    private List<WebElement> addToCartButtons; // List for "Add to Cart" buttons
+
+    @FindBy(className = "shopping_cart_badge")
+    private WebElement cartBadge;
+    @FindBy(css = ".inventory_item:nth-child(1) .btn_inventory")
+    private WebElement addToCartButtonForFirstItem;
+
+    @FindBy(css = ".inventory_item:nth-child(1) .btn_secondary")
+    private WebElement removeFromCartButtonForFirstItem;
     public ProductsPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
@@ -16,4 +28,24 @@ public class ProductsPage {
     public String getPageTitle() {
         return pageTitle.getText();
 }
+
+    public void addFirstItemToCart() {
+        if (!addToCartButtons.isEmpty()) {
+            addToCartButtons.get(0).click(); // Click on the first "Add to cart" button
+        }
+    }
+
+    public void addSecondItemToCart() {
+        if (addToCartButtons.size() > 1) {
+            addToCartButtons.get(1).click(); // Click on the second "Add to cart" button
+        }
+    }
+
+    public String getCartBadgeCount() {
+        return cartBadge.isDisplayed() ? cartBadge.getText() : "0";
+    }
+
+    public boolean isPageLoaded() {
+        return pageTitle.isDisplayed() && pageTitle.getText().equals("Products");
+    }
 }
--- UI_Testing/src/test/resources/features/products.feature
+++ UI_Testing/src/test/resources/features/products.feature
@@ -6,3 +6,19 @@ Feature: Products Page Functionality
   Scenario: Verify products page title
     Given I am on the products page
     Then the page title should be "Products"
+
+
+    @smoke @functional
+    Scenario: Add a single item to the cart
+      Given I am on the products page
+      When I add the first item to the cart
+      Then the cart badge count should be "1"
+
+
+    @smoke @functional
+    Scenario: Add multiple items to the cart
+      Given I am on the products page
+      When I add the first item to the cart
+      And I add the second item to the cart
+      Then the cart badge count should be "2"
+
